8ac9305f4e70b5d1b93151c599c73389fc4f45c8
wchaochao <1244210319@qq.com>
2020-01-02 06:21:54 +0800

    添加webpack配置

--- main.js
+++ main.js
@@ -1,17 +1,1 @@
-const readline = require('readline')
-const Lexer = require('./Lexer')
-const Parser = require('./Parser')
-const Interpreter = require('./Interpreter')
-
-const rl = readline.createInterface(process.stdin, process.stdout)
-rl.setPrompt('calc> ')
-rl.prompt()
-
-rl.on('line', line => {
-  const lexer = new Lexer(line)
-  const parser = new Parser(lexer)
-  const interpreter = new Interpreter(parser)
-  let result = interpreter.interprete()
-  console.log(result)
-  rl.prompt()
-})
+import * as example from './src/examples/symbol'
