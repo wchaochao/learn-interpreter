3e3a7e05875a88a7c82dd17a17edd874c53b86d4
wchaochao <1244210319@qq.com>
2020-01-10 22:16:11 +0800

    修改webpack打包配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,41 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+
+const isDev = process.env.NODE_ENV === 'development'
+console.log(process.env.NODE_ENV)
+const otherPlugins = isDev ? [new BundleAnalyzerPlugin()] : []
+
 module.exports = {
   entry: './main.js',
   output: {
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'learn-webpack',
+      filename: 'index.html',
+      template: 'index.html'
+    }),
+    ...otherPlugins
+  ],
+  devServer: {
+    contentBase: './dist',
+    open: true
   }
 }
